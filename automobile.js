213675e4184a3ce50315c433eece48d8e6a01586
Derek <drkgrntt@gmail.com>
2017-09-09 15:53:26 -0500

    Animate vehicle movement
    # Conflicts:
    #	main.js

--- automobile.js
+++ automobile.js
@@ -1,15 +1,55 @@
+// directions
+const N = 0;
+const E = 1;
+const S = 2;
+const W = 3;
+
 class automobile 
 {
-  constructor(x, y, w, h)
+  // x, y, width, height, speed, direction
+  constructor(x, y, w, h, s, d)
   {
     this.x = x;
     this.y = y;
-    this.w = w;
-    this.h = h;
+    this.s = s;
+    this.d = d;
+    this.isOffScreen = false;
+    // if direction is North or South, vehicle faces North or South
+    if (d === N || d === S) {
+      this.w = w;
+      this.h = h;
+    // if direction is East or West, vehicle faces East or West
+    } else if (d === E || d === W) {
+      this.w = h;
+      this.h = w;
+    }
   }
         
   draw()
   {
-    rect(this.x, this.y, this.w, this.h);
+    // vehicle
+    rect(this.x, this.y, this.w, this.h, this.s, this.d);
+  }
+
+  drive()
+  {
+    // move vehicle along x/y according to direction
+    if (this.d === N) {
+      this.y -= this.s
+    } else if (this.d === E) {
+      this.x += this.s
+    } else if (this.d === S) {
+      this.y += this.s
+    } else if (this.d === W) {
+      this.x -= this.s
+    }
+
+    // detect if car is off screen
+    if ( this.x > window.innerWidth ||
+         this.x < 0 - this.w ||
+         this.y > window.innerHeight ||
+         this.y < 0 - this.h ) {
+      this.isOffScreen = true;
+    }
   }
 }
--- main.js
+++ main.js
@@ -3,16 +3,23 @@ var canv;
 
 function setup() {
   canv = createCanvas(window.innerWidth, window.innerHeight);
-  console.log("Setup Run")
-  //car = new automobile(10,10,50,50);
+  car = new automobile(10,10,30,50,3,S);
   road1 = new road( 65, 0, 3, window.innerHeight, NS );
 }
 
 function draw() {
-  background(51);
+  background(0,200,0);
   fill(255);
-  //car.draw();
-  road1.draw()
+  if(car) 
+  {
+    car.draw();
+    car.drive();
+    if(car.isOffScreen) {
+      car = null;
+      console.log("car deleted");
+    }
+  }
+  road1.draw();
 }
 
 function assert( condition, message )
