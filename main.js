6b3d71c9e48c48bef889d4e776028736132d2b19
Caleb <clbgrmn@gmail.com>
2017-09-09 14:10:50 -0500

    Tab cleanup and road direction handling.

--- main.js
+++ main.js
@@ -2,13 +2,23 @@
 
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-    console.log("test")
-    car = new automobile(10,10,50,50);
+  createCanvas(window.innerWidth, window.innerHeight);
+  console.log("test")
+  car = new automobile(10,10,50,50);
 }
 
 function draw() {
-      background(51);
-      fill(255);
-      car.draw();
+  background(51);
+  fill(255);
+  car.draw();
+}
+
+function assert( condition, message )
+{
+  if (!condition) {
+    message = message || "Assertion failed";
+    if (typeof Error !== "undefined") {
+      throw new Error(message);
+  }
+  throw message; // Fallback
 }
--- road.js
+++ road.js
@@ -4,36 +4,43 @@ const laneW = 10;
 const stripeW = 1;
 const stripeH = 4;
 
-const LEFT = 0;
-const RIGHT = 1;
+const NS = 0;
+const EW = 1;
 
 class road
+{
+  constructor(x, y, numLanes, len, dir)
+  {
+    //Road width is: Number of lanes times width of lane + width the outside road stripes + the width of the stripes between the lanes
+    roadW = numLanes * laneW + 2 * stripeW + ( numLanes - 1 ) * stripeW;
+
+    this.dir = dir;
+    this.x = x;
+    this.y = y;
+
+    if( dir == NS )
     {
-    constructor(x, y, numLanes, len, dir)
-        {
-            //Road width is: Number of lanes times width of lane + width the outside road stripes + the width of the stripes between the lanes
-            roadW = numLanes * laneW + 2 * stripeW + ( numLanes - 1 ) * stripeW;
-
-            this.x = x;
-            this.y = y;
-
-            if( dir == roadDir.LEFT )
-            {
-
-            }
-            this.h = h;
-        }
-        
-    draw()
-        {
-            rect(this.x, this.y, this.w, this.h);
-            drawStripes()
-        }
-        
-    drawStripes()
-        {
-            //Draw outside stripes
-            fill(244, 235, 66)
-            rect(this.x, this.y, )
-        }
+    this.w = roadW;
+    this.h = len; 
     }
+    else
+    {
+      assert( dir == EW, "Unexpected direction." )
+      this.w = len;
+      this.h = roadW;
+    }
+  }
+
+  draw()
+  {
+    rect(this.x, this.y, this.w, this.h);
+    drawStripes()
+  }
+
+  drawStripes()
+  {
+    //Draw outside stripes
+    fill(244, 235, 66)
+    rect(this.x, this.y, )
+  }
+}
