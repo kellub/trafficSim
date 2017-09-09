7d0fb2704c64cf04218d3b9837e3608c0ac96e91
Derek <drkgrntt@gmail.com>
2017-09-09 14:16:26 -0500

    cleanup automobile.js

--- automobile.js
+++ automobile.js
@@ -1,15 +1,15 @@
-class automobile
-    {
-    constructor(x, y, w, h)
-        {
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            this.h = h;
-        }
+class automobile 
+{
+  constructor(x, y, w, h)
+  {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
         
-    draw()
-        {
-            rect(this.x, this.y, this.w, this.h);
-        }
-    }
+  draw()
+  {
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
