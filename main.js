28a461aa2f2b9a5fb195e26bc1d580ac057564da
Derek <drkgrntt@gmail.com>
2017-09-09 19:41:11 -0500

    add cars and arrange cars and roads

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ var canv;
 
 function setup() {
   canv = createCanvas(window.innerWidth, window.innerHeight);
-  car = new automobile(10,10,30,50,3,S);
-  car = new automobile(71,10,30,50,3,S);
-  road1 = new road( 65, 0, 3, window.innerHeight, NS );
-  road2 = new road( 0, 65, 3, window.innerWidth, EW );
+  truck = new automobile(71,0,30,50,3,S);
+  sedan = new automobile(window.innerWidth,115,25,44,4,W);
+  coup = new automobile(0,76,20,37,5,E);
+  road1 = new road( 65, 0, 2, window.innerHeight, NS );
+  road2 = new road( 0, 65, 2, window.innerWidth, EW );
 }
 
 function draw() {
@@ -14,16 +15,35 @@ function draw() {
   fill(255);
   road1.draw();
   // ensure roads are drawn before cars so they don't render over cars
-  if(car) 
+  if(truck) 
   {
-    car.draw();
-    car.drive();
-    if(car.isOffScreen) {
-      car = null;
-      console.log("car deleted");
+    truck.draw();
+    truck.drive();
+    if(truck.isOffScreen) {
+      truck = null;
+      console.log("truck deleted");
     }
   }
+
   road2.draw();
+  if(coup) 
+  {
+    coup.draw();
+    coup.drive();
+    if(coup.isOffScreen) {
+      coup = null;
+      console.log("coup deleted");
+    }
+  }
+  if(sedan) 
+  {
+    sedan.draw();
+    sedan.drive();
+    if(sedan.isOffScreen) {
+      sedan = null;
+      console.log("sedan deleted");
+    }
+  }
 }
 
 function assert( condition, message )
